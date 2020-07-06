e3cbe5d49e9c0c23ea4e69dc8ba5cfc03aa5ced2
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2020-07-06 23:25:13 +0530

    Initial Commit

---
+++ PreFetchLoading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PreFetchLoading
+//
+//  Created by MACBOOK on 06/07/20.
+//  Copyright © 2020 SukhmaniKaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
